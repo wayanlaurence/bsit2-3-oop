27c0269f5b3b0b3cbeb869b2eb3a67117b51f151
wayanlaurence <rljsatur28301@liceo.edu.ph>
2025-08-07 16:21:23 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,15 @@
+
 public class Main {
-
-
-    static class Student {
-
-        private final String name;
-        private final int age;
-        private final String course;
-        private final double grade1;
-        private final double grade2;
-        private final double grade3;
-
-
-        public Student(String name, int age, String course, double grade1, double grade2, double grade3) {
-            this.name = name;
-            this.age = age;
-            this.course = course;
-            this.grade1 = grade1;
-            this.grade2 = grade2;
-            this.grade3 = grade3;
-        }
-
-
-        public void displayInfo() {
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-            System.out.println("Course: " + course);
-        }
-
-
-        public double calculateAverage() {
-            return (grade1 + grade2 + grade3) / 3;
-        }
-
-
-        public String getLetterGrade() {
-            double avg = calculateAverage();
-            if (avg >= 90) return "A";
-            else if (avg >= 80) return "B";
-            else if (avg >= 70) return "C";
-            else if (avg >= 60) return "D";
-            else return "F";
-        }
-
-
-        public boolean isPassing() {
-            return calculateAverage() >= 70;
-        }
-    }
-
-
     public static void main(String[] args) {
-
-
-        Student s1 = new Student("Ar pagums", 20, "Computer Science", 85, 90, 88);
-        Student s2 = new Student("lala", 22, "Information Technology", 0, 70, 72);
-        Student s3 = new Student("zaza", 21, "Engineering", 85, 80, 78);
+        
+        Student s1 = new Student("Ar pags", 20, "Computer Science", 85, 90, 88);
+        Student s2 = new Student("Bob agus", 22, "Information Technology", 70, 65, 72);
+        Student s3 = new Student("Charlie Blue", 21, "Engineering", 55, 60, 58);
 
         Student[] students = {s1, s2, s3};
         int passingCount = 0;
 
-
+        
         for (Student s : students) {
             System.out.println("------------");
             s.displayInfo();
@@ -70,9 +20,8 @@ public class Main {
             if (s.isPassing()) passingCount++;
         }
 
-
+        
         System.out.println("------------");
         System.out.println("Total number of PASSING students: " + passingCount);
     }
 }
-
